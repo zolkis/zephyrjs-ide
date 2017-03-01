bbf8cd35e895c99e268ca3d8dd64c8f7343c2210
Salvatore Iovene <salvatore.iovene@intel.com>
2017-03-01 15:36:51 +0200

    chore(tests): add e2e test for #49

--- src/client/app/app.component.e2e-spec.ts
+++ src/client/app/app.component.e2e-spec.ts
@@ -11,4 +11,26 @@ describe('App', () => {
         expect(element(by.css('sd-navbar nav')).isPresent()).toEqual(true);
     });
 
+    it('routing should preserve editor tabs', () => {
+        let tabs;
+
+        // Initial check
+        browser.ignoreSynchronization = true;
+        browser.get('/#/editor');
+        tabs = element.all(by.css('sd-editor .left-component a.nav-link'));
+        expect(tabs.count()).toEqual(1);
+
+        // Add a tab
+        element(by.id('new-tab-button')).click();
+        tabs = element.all(by.css('sd-editor .left-component a.nav-link'));
+        expect(tabs.count()).toEqual(2);
+
+        // Route to About
+        element(by.css('sd-navbar .navbar-right li:nth-child(1) a')).click();
+
+        // Route back to Editor
+        element(by.css('sd-navbar .navbar-right li:nth-child(2) a')).click();
+        tabs = element.all(by.css('sd-editor .left-component a.nav-link'));
+        expect(tabs.count()).toEqual(2);
+    });
 });
