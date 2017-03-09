c146df0cf6c9e0379d6e4ade39481a819838862b
Salvatore Iovene <salvatore@iovene.com>
2017-03-09 13:09:39 +0200

    chore(editor): add e2e test for adding/removing tabs

--- src/client/app/pages/editor/editor.component.e2e-spec.ts
+++ src/client/app/pages/editor/editor.component.e2e-spec.ts
@@ -15,4 +15,21 @@ describe('Editor', () => {
         let el = element(by.css('sd-navbar .navbar-right li:nth-child(2) a'));
         expect(el.getText()).toEqual('Editor');
     });
+
+    it('adding/removing tabs should work', (done) => {
+        let btn = element(by.id('new-tab-button'));
+        btn.click().then(() => {
+            let tablist = element.all(by.css(
+                '.split-pane-content-primary .nav-tabs .nav-item'));
+            expect(tablist.count()).toBe(2);
+
+            let closeTabBtn = element(by.css(
+                '.split-pane-content-primary .nav-tabs ' +
+                '.nav-item:last-child .close-tab'));
+            closeTabBtn.click().then(() => {
+                expect(tablist.count()).toBe(1);
+                done();
+            });
+        });
+    });
 });
