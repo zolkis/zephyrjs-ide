3a9951572f0fad58aa998a9d862e3b6164575c3d
Salvatore Iovene <salvatore@iovene.com>
2017-06-22 14:36:47 +0200

    fix(board-explorer): use relative paths for boards
    This allows builds with APP_BASE !== '/'.

--- src/client/app/pages/editor/components/board-explorer/board-explorer.service.ts
+++ src/client/app/pages/editor/components/board-explorer/board-explorer.service.ts
@@ -8,7 +8,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class BoardExplorerService {
-    private readonly BASE_PATH: string = '/assets/boards/';
+    private readonly BASE_PATH: string = 'assets/boards/';
 
     private _boards: string[] = [
         'arduino_101',
